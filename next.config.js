f1cbb27b7ad525b8d9771aea0ea4148855a057ec
sangkukbae <sangkukbae@gmail.com>
2024-04-24 23:15:06 +0900

    Add CDN configuration for medusa-store.sgp1.cdn.digitaloceanspaces.com

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "medusa-store.sgp1.cdn.digitaloceanspaces.com",
+      },
     ],
   },
 })
